6a421532dc948af36f8ac984646bc6700b1e7c70
jack <1458583930@qq.com>
2019-02-26 16:12:00 +0800

    新增商品详情页

---
+++ 弩币商城/Home/View/BCCouponView.h
@@ -0,0 +1,19 @@
+//
+//  BCCouponView.h
+//  弩币商城
+//
+//  Created by Jack on 2019/2/26.
+//  Copyright © 2019年 詹姆斯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BCCouponView : UIView
+
+@property (nonatomic,copy)void(^backBlock)(void);
+- (instancetype)initWithFrame:(CGRect)frame andUserID:(NSString *)ID;
+@end
+
+NS_ASSUME_NONNULL_END
