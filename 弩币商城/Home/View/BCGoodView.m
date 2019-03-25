f8f9d2e8f93bffde5417a884354668117c134def
jack <1458583930@qq.com>
2019-03-25 11:07:50 +0800

    修改bug

--- 弩币商城/Home/View/BCGoodView.m
+++ 弩币商城/Home/View/BCGoodView.m
@@ -154,15 +154,15 @@
     moneyL1.textColor = COLOR(254, 0, 0);
     moneyL1.font = Regular(13);
      moneyL1.attributedText = str;
-    
-    if ([[paramS objectForKey:@"q_fenqi"] integerValue] == 1) {//分期
-        [headView addSubview:moneyL1];
-        [moneyL1 mas_makeConstraints:^(MASConstraintMaker *make) {
-            
-            make.left.equalTo(moneyL.mas_left);
-            make.top.equalTo(moneyL.mas_bottom).offset(12);
-            make.height.mas_equalTo(12);
-        }];
+    [headView addSubview:moneyL1];
+    [moneyL1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.left.equalTo(moneyL.mas_left);
+        make.top.equalTo(moneyL.mas_bottom).offset(12);
+        make.height.mas_equalTo(12);
+    }];
+    if ([[paramS objectForKey:@"q_fenqi"] integerValue] != 1) {//分期
+        moneyL1.hidden = YES;
     }
     
    
