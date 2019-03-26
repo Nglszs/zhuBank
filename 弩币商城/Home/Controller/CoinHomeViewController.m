c810943cfc63ee2827e508a9930fe037056ea2ec
jack <1458583930@qq.com>
2019-03-26 10:54:53 +0800

    修改bug

--- 弩币商城/Home/Controller/CoinHomeViewController.m
+++ 弩币商城/Home/Controller/CoinHomeViewController.m
@@ -61,12 +61,12 @@
         
     } else {
         
-        WOWONoDataView *view = [[WOWONoDataView alloc] initWithImageName:@"order" text:@"网络似乎出现了问题" detailText:nil buttonTitle:@"点击刷新"];
-        [self.view addSubview:view];
-        [view.button addtargetBlock:^(UIButton *button) {
-           
-            [self getData];
-        }];
+//        WOWONoDataView *view = [[WOWONoDataView alloc] initWithImageName:@"order" text:@"网络似乎出现了问题" detailText:nil buttonTitle:@"点击刷新"];
+//        [self.view addSubview:view];
+//        [view.button addtargetBlock:^(UIButton *button) {
+//
+//            [self getData];
+//        }];
         
         
     }
