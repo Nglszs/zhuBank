a6e1b9e00a05e79f655d7500c31c2d3c678c7f9f
jack <1458583930@qq.com>
2019-03-29 09:30:41 +0800

    修改bug

--- 弩币商城/PersonCenter/CoinPersonViewController.m
+++ 弩币商城/PersonCenter/CoinPersonViewController.m
@@ -22,6 +22,8 @@
 #import "CoinMemberViewController.h"
 #import "CoinMemberBuyViewController.h"
 #import "CoinCertifyViewController.h"
+#import "CoinLoginViewController.h"
+#import "BCNavigationViewController.h"
 
 @interface CoinPersonViewController ()
 {
@@ -47,6 +49,20 @@
     
     [self.navigationController setNavigationBarHidden:NO animated:animated];
     
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+    if (![Tool isLogin]) {
+        CoinLoginViewController *workVC =  [[CoinLoginViewController alloc] init];
+        NSMutableArray *arr = [[NSMutableArray alloc] initWithArray:[self.tabBarController viewControllers]];
+        BCNavigationViewController *workNav = [[BCNavigationViewController alloc] initWithRootViewController:workVC];
+        workNav.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"我的" image:[[UIImage imageNamed:@"我的 (1)"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] selectedImage:[[UIImage imageNamed:@"我的2 (1)"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
+        [arr replaceObjectAtIndex:3 withObject:workNav];
+        [self.tabBarController setViewControllers:arr];
+    }
+   
 }
 
 - (void)viewDidLoad {
